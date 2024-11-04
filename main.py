932f3d5c3df406099158d736a36c7ea48af72e5e
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-11-04 16:29:59 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ WORK_TIME = 25
 SHORT_BREAK = 5
 LONG_BREAK = 30
 reps = 1
-mark = " "
+mark = ""
 timer = None
 
 # TIMER RESET
@@ -23,9 +23,10 @@ def reset_timer():
     window.after_cancel(timer)
     title_label.config(text="Timer", fg=GREEN)
     reps = 1
-    mark = " "
+    mark = ""
     checkmarks.config(text=mark)
     canvas.itemconfig(timer_text, text="00:00")
+    start_button.config(state="active")
 
 
 # TIMER MECHANISM
@@ -46,6 +47,7 @@ def start_timer():
         countdown(long_break_secs)
         title_label.config(text="Break", fg=RED)
 
+    start_button.config(state="disabled")
 
 # COUNTDOWN MECHANISM
 def countdown(count):
@@ -72,13 +74,10 @@ def countdown(count):
         checkmarks.config(text=mark)
         start_timer()
 
-
-
 # UI SETUP
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=50, pady=25, bg=YELLOW)
-
 
 canvas = Canvas(width=520, height=520, bg=YELLOW, highlightthickness=0)
 Tomato_image = PhotoImage(file="Tomato.png")
@@ -86,17 +85,16 @@ canvas.create_image(260, 260, image=Tomato_image)
 timer_text = canvas.create_text(260, 265, text="00:00", fill="white", font=(FONT_NAME, 40, "bold"))
 canvas.grid(column=1, row=1)
 
-
 title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45, "bold"))
 title_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", font=(FONT_NAME, 20, "bold"), command=start_timer)
+start_button = Button(text="Start", font=(FONT_NAME, 20, "bold"), state="active", command=start_timer)
 start_button.grid(column=0, row=2)
 
 checkmarks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
 checkmarks.grid(column=1, row=3)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 20, "bold"), highlightthickness=0, command=reset_timer)
+reset_button = Button(text="Reset", font=(FONT_NAME, 20, "bold"), command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
